5880a611d635f53c9fb7e89eafde9459add9fc92
chinosk <2248589280@qq.com>
2022-07-19 00:21:16 +0800

    添加图片信息和发送失败提示

--- bot/plugins/setu/setumain.py
+++ bot/plugins/setu/setumain.py
@@ -11,8 +11,12 @@ def on_message(event: models.Message):
     if event.message.startswith("来张") and event.message.endswith("色图"):
         try:
             print(f"{event.user_id} 请求色图: {event.message}")
-            send = get_setu(event.message)
-            bot_api.reply_message(event, send)
+            info, image = get_setu(event.message)
+            send = f"{info}\n{image}"
+            ret = bot_api.reply_message(event, send)
+            ret_data = json.loads(ret)
+            if ret_data["retcode"] != 0:
+                bot_api.reply_message(event, f"图片发送失败\n{info}")
         except BaseException as e:
             bot_api.reply_message(event, f"获取失败: {e}")
 
@@ -29,10 +33,13 @@ def get_setu(msg: str):
     data = json.loads(setu_api.lolicon(r18, tags))
     try:
         img_url = data["data"][0]["urls"]["original"]
+        img_pid = data["data"][0]["pid"]
+        img_tags = ", ".join(data["data"][0]["tags"])
+        img_tags = img_tags[:50]
         img_path = setu_api.download_image(img_url)
-        return f"[随机图片]\n{bot_api.GeneCQCode.image(img_path, is_file=True)}"
-    except:
+        return f"PID: {img_pid}\ntags: {img_tags}", bot_api.GeneCQCode.image(img_path, is_file=True)
+    except BaseException as e:
         if "error" in data and data["error"] != "":
             return f"获取失败: {data['error']}"
         else:
-            return "获取失败"
+            raise e
